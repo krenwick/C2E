7457e774792aa7e489ae956128b8ed44b41d322e
krenwick <katie.renwick@gmail.com>
2017-10-21 10:20:20 -0600

    Initial code commit

---
+++ Code/convert_driver_data.R
@@ -0,0 +1,97 @@
+################################################################################
+# Convert driver data into format needed for LPJ-GUESS
+# Original data files provided by Kevin
+################################################################################
+rm(list=ls())
+library(tidyverse); theme_set(theme_bw(base_size=12)) # sized for print
+
+# Set working directory:
+setwd("~/Documents/C2E/Data")
+
+# Coordinate of study site:
+ll <- c(39.11, -96.61)
+
+############ Read in data and convert to monthly ###############################
+ambient <- read.csv("knz_amb_forcing_91-12.csv")
+irr <- read.csv("knz_irr_forcing_91-12.csv")
+
+ambient2 <- ambient %>% 
+  mutate(date=format(strptime(paste(year, doy), format="%Y %j"))) %>%
+  group_by(year,doy,date) %>% # group by day to calculate days with precip
+  summarise(dayppt=sum(precp), daytemp=mean(tair), daysol=mean(rad_h)) %>%
+  mutate(month=format(as.Date(date), "%m")) %>%
+  group_by(year, month) %>%
+  summarise(temp=mean(daytemp), ppt=sum(dayppt), meansol=mean(daysol),
+            wet=sum(dayppt>0)) 
+irr2 <- irr %>%
+  mutate(date=format(strptime(paste(year, doy), format="%Y %j"))) %>%
+  group_by(year,doy,date) %>% # group by day to calculate days with precip
+  summarise(dayppt=sum(precp), daytemp=mean(tair), daysol=mean(rad_h)) %>%
+  mutate(month=format(as.Date(date), "%m")) %>%
+  group_by(year, month) %>%
+  summarise(temp=mean(daytemp), ppt=sum(dayppt), meansol=mean(daysol),
+            wet=sum(dayppt>0)) 
+
+ambient2$lon <- ll[2]
+ambient2$lat <-ll[1]
+irr2$lon <- ll[2]
+irr2$lat <- ll[1]
+
+# Function to take data and put into form for LPJ-GUESS read-in
+# Data must be long-form, include lat-lon, and have variables as columns
+# nyear: number of simulation years desired, ... = variable to use
+format_guess <- function(data, nyear, ...){ # use dots since passing text to dplyr problematic
+  d1 <- data %>% select(lon, lat, year, month, ...) %>%
+    spread(month, ...)
+  # Round up then chop off first few rows to get right #
+  d2 <- do.call(rbind, replicate(ceiling(nyear/nrow(d1)), d1, simplify=FALSE))
+  s <- (nrow(d2) - nyear)
+  return(d2[(s+1):nrow(d2),])
+}
+
+# I have 22 years of site-specific climate data (1991-2012)
+# We agreed to do a 1000-year spin-up, then 1860-1990 with repeated met (131 yr)
+# and increasing CO2. So I need 1000+131+22=1153 yrs
+temp <- format_guess(ambient2,1153, temp)
+srad <- format_guess(ambient2,1153, meansol)
+ppt <- format_guess(ambient2,1153, ppt)
+wetd <- format_guess(ambient2,1153, wet)
+ppt_irr <- format_guess(irr2,1153, ppt)
+wetd_irr <- format_guess(irr2,1153, wet)
+
+# But wait! I actually want spin-up to be ambient, not irrigation:
+ppt_irr[1:(nrow(ppt_irr)-22),] <- ppt[1:(nrow(ppt_irr)-22),]
+wetd_irr[1:(nrow(ppt_irr)-22),] <- wetd[1:(nrow(ppt_irr)-22),]
+
+
+write.table(temp, "temp.txt", sep=" ",
+            quote=F, row.names=F, col.names=F)
+write.table(ppt, "ppt.txt", sep=" ",
+            quote=F, row.names=F, col.names=F)
+write.table(srad, "srad.txt", sep=" ",
+            quote=F, row.names=F, col.names=F)
+write.table(wetd, "wetd.txt", sep=" ",
+            quote=F, row.names=F, col.names=F)
+write.table(ppt_irr, "ppt_irr.txt", sep=" ",
+            quote=F, row.names=F, col.names=F)
+write.table(wetd_irr, "wetd_irr.txt", sep=" ",
+            quote=F, row.names=F, col.names=F)
+
+################################################################################
+# Combine driver files into 1 so can run both experiments at once?
+
+################################################################################
+# Convert CO2 data into correct format
+################################################################################
+c1 <- read.table("global_co2_ann_1860_2016.txt",header=F)
+head(c1) # 1860 = 286.42, data through 2016! We want through 2012
+c2 <- filter(c1, V1<=2012)
+yr <- seq(1,1153,1)
+co <- rep(286,1000)
+co2 <- c(co,c2$V2)
+df <- cbind.data.frame(yr,co2)
+
+# Write:
+write.table(df, "co2_C2E.txt",quote=F,row.names=F,col.names=F)
+
+
---
+++ Code/plot_output.R
@@ -0,0 +1,220 @@
+################################################################################
+# Plots of model output for the C2E project
+################################################################################
+rm(list=ls()) 
+library(tidyverse); theme_set(theme_bw(base_size=18))
+library(data.table)
+library(zoo)
+
+# Set working directory
+setwd("~/Documents/C2E/Output")
+
+# Read in reference data from Konza:
+k <- read.csv("../Data/RefData/anpp_1991_2012.csv")
+head(k)
+
+# Read in model output (monthly) ------------------------------------
+# Pull in monthly data from orig1
+orig1 <- NULL 
+vars=c("mgpp","mrh","mra","mnee","mevap","maet","mlai","mwcont_upper")
+for(var in vars) {
+  data=paste("orig1_",var,".out", sep="")
+  b <- fread(data, header=T)
+  b1 <- b %>% mutate(Year=Year+860) %>% 
+    filter(Year>=1991) %>%
+    gather(Month,orig1, Jan:Dec) %>%
+    mutate(Variable=var) %>%
+    select(Year, Month,Variable,orig1)
+  orig1 <- rbind.data.frame(orig1,b1)
+}
+
+orig2 <- NULL 
+for(var in vars) {
+  data=paste("orig2_",var,".out", sep="")
+  b <- fread(data, header=T)
+  b1 <- b %>% mutate(Year=Year+860) %>% 
+    filter(Year>=1991) %>%
+    gather(Month,orig2, Jan:Dec) %>%
+    mutate(Variable=var) %>%
+    select(Year, Month,Variable,orig2)
+  orig2 <- rbind.data.frame(orig2,b1)
+}
+
+orig3 <- NULL 
+for(var in vars) {
+  data=paste("orig3_",var,".out", sep="")
+  b <- fread(data, header=T)
+  b1 <- b %>% mutate(Year=Year+860) %>% 
+    filter(Year>=1991) %>%
+    gather(Month,orig3, Jan:Dec) %>%
+    mutate(Variable=var) %>%
+    select(Year, Month,Variable,orig3)
+  orig3 <- rbind.data.frame(orig3,b1)
+}
+
+origs <- merge(orig1, orig2, by=c("Year", "Month","Variable")) %>%
+  merge(.,orig3,by=c("Year", "Month","Variable")) %>%
+  gather(Model,Value,orig1:orig3)
+  
+head(origs)
+origs$D <- as.yearmon(paste(origs$Year, origs$Month), "%Y %b")
+origs$Date <- as.Date(origs$D)
+
+################################################################################
+# Plot annual NPP with reference conditions
+################################################################################
+o1 <- read.table("orig1_anpp.out", header=T) %>%
+  mutate(Model="orig1")
+o2 <- read.table("orig2_anpp.out", header=T) %>%
+  mutate(Model="orig2")
+o3 <- read.table("orig3_anpp.out", header=T) %>%
+  mutate(Model="orig3")
+
+npp <- rbind.data.frame(o1,o2,o3) %>%
+  mutate_at(vars(ANGE:Total), funs(.*2000)) %>%
+  mutate(year=Year+860) %>%
+  filter(year>=1991) %>%
+  select(year,Model,Total) %>%
+  mutate(Total=Total*0.4761905) %>% # so just above-ground
+  rename(Source=Model, NPP=Total)
+
+knpp <- k %>% rename(Source=treatment, NPP=anpp_gm2) %>%
+  select(year,Source,NPP)
+npp1 <- rbind(npp,knpp) %>%
+  filter(Source!="orig2")
+
+ggplot(data=npp1, aes(x=year, y=NPP, color=Source)) +
+  geom_point() +
+  geom_line()
+
+################################################################################
+# Plot annual FPC by species
+################################################################################
+fpc1 <- read.table("orig1_fpc.out", header=T) %>%
+  mutate(Model="orig1")
+fpc2 <- read.table("orig2_fpc.out", header=T) %>%
+  mutate(Model="orig2")
+fpc3 <- read.table("orig3_fpc.out", header=T) %>%
+  mutate(Model="orig3")
+
+fpc <- rbind.data.frame(fpc1,fpc2,fpc3) %>%
+  mutate(Year=Year+860) %>%
+  filter(Year>=1980) %>%
+  gather(Species, FPC, ANGE:PAVI) %>%
+  filter(Model != "orig2")
+
+# (tried full time series- converges early)
+ggplot(data=fpc, aes(x=Year,y=FPC, color=Model, linetype=Species)) +
+  geom_line() + 
+  geom_point()
+
+# Plot together, just orig3, to see if equal 100:
+ggplot(data=fpc, aes(x=Year,y=FPC,fill=Species)) +
+  geom_area(position="stack") +
+  facet_wrap(~Model)
+# Total FPC is ~60%
+
+################################################################################
+# Compare biomass output to reference
+################################################################################
+# b1 <- read.table("orig1_cmass.out", header=T) %>%
+#   mutate(Model="orig1")
+# b2 <- read.table("orig2_cmass.out", header=T) %>%
+#   mutate(Model="orig2")
+# b3 <- read.table("orig3_cmass.out", header=T) %>%
+#   mutate(Model="orig3")
+# 
+# bio <- rbind.data.frame(b1,b2,b3) %>%
+#   mutate(Year=Year+860) %>%
+#   filter(Year>=1991) %>%
+#   group_by(Model) %>%
+#   summarise_at(vars(ANGE:Total),mean)
+# # Total biomass should be  .5673 kgC/m2
+# # I get .273-.31 (should I double?)
+# 
+# # Try cpools -----------------------------------------
+# cp1 <- read.table("orig1_cpool.out", header=T) %>%
+#   mutate(Model="orig1")
+# cp2 <- read.table("orig2_cpool.out", header=T) %>%
+#   mutate(Model="orig2")
+# cp3 <- read.table("orig3_cpool.out", header=T) %>%
+#   mutate(Model="orig3")
+# 
+# cp <- rbind.data.frame(cp1,cp2,cp3) %>%
+#   mutate(Year=Year+860) %>%
+#   filter(Year>=1991) %>%
+#   group_by(Model) %>%
+#   summarise_at(vars(VegC:Total),mean)
+# # -	Soil carbon pools should be: (9kgC/m2 total)
+# cp # I get less-not sure what numbers to compare though
+
+################################################################################
+# Compare soil moisture (upper layer) output to reference
+################################################################################
+mwc <- filter(origs,Variable=="mwcont_upper") %>%
+  rename(Source=Model) %>%
+  select(Year:Value, -Variable) %>%
+  mutate(Value=Value*.23) %>%
+  filter(Source!="orig2")
+
+# read in reference data and aggregate to monthly
+m <- read.csv("../Data/RefData/soil_moisture_knz_0-15cm_2007_2012_daily.csv") %>%
+  group_by(year,month,transect) %>%
+  summarise(Value=mean(soil_moist_volumetric)) %>%
+  mutate(Source=ifelse(transect=="control","Ambient","Irrigation")) %>%
+  #filter(transect=="control") %>%
+  ungroup() %>%
+  mutate(month = month.abb[month]) %>%
+  rename(Year=year, Month=month) %>%
+  select(-transect)
+
+sm <- merge(mwc,m, by=c("Year","Month","Source","Value"), all=T) %>%
+  select(Year:Value) %>%
+  filter(Year>=2005) %>%
+  spread(Source,Value) %>%
+  na.omit() %>%
+  gather(Source,Value,Ambient:orig3) %>%
+  arrange(Year,Month)
+sm$D <- as.yearmon(paste(sm$Year, sm$Month), "%Y %b")
+sm$Date <- as.Date(sm$D)
+sm2 <- sm %>% arrange(Date)
+# Date <- as.data.frame(seq.Date(as.Date("2007-05-01"), as.Date("2012-12-01"), 
+#                                "month"))
+# names(Date) <- c("Date")
+# sm2 <- merge(Date,sm, by="Date", all.x=T) %>%
+#   filter(Year>2009)
+
+idx <- c(1, diff(sm2$Date))
+i2 <- c(1,which(idx > 31), nrow(sm2)+1)
+sm2$grp <- rep(1:length(diff(i2)), diff(i2)) 
+sm2$g <- paste(sm2$grp,sm2$Source)
+
+sm3 <- filter(sm2, Year>2010)
+ggplot(data=sm3, aes(x=Date, y=Value, color=Source)) +
+  geom_point() +
+  geom_line(aes(group = g)) +
+  scale_x_date(date_breaks = "6 months",date_labels = "%Y-%b") 
+
+################################################################################
+# Is there something weird going on with my precip data?
+################################################################################
+amb <- read.table("../Data/ppt.txt", header=F)
+irr <- read.table("../Data/ppt_irr.txt")
+head(amb)
+names(amb) <- c("Lat","Lon","Year",seq_along(month.abb))
+amb2 <- amb %>% mutate(yr=seq(1:nrow(amb))) %>%
+  mutate(Year=yr+859) %>%
+  gather(Month,Ambient,`1`:`12`)
+names(irr) <- c("Lat","Lon","Year",seq_along(month.abb))
+irr2 <- irr %>% mutate(yr=seq(1:nrow(irr))) %>%
+  mutate(Year=yr+859) %>%
+  gather(Month,Irrigated,`1`:`12`)
+ppt <- merge(amb2,irr2,by=c("Lat","Lon","Year","Month")) %>%
+  filter(Year>2005)
+ppt$D <- as.yearmon(paste(ppt$Year, ppt$Month), "%Y %m")
+ppt$Date <- as.Date(ppt$D)
+ggplot(data=ppt, aes(x=Date,y=Ambient)) +
+  #geom_point() +
+  geom_line(color="red") +
+  geom_line(aes(x=Date,y=Irrigated), color="blue")
+  
